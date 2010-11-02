b610d7630ebe1bb06ae4c9768151f6cc43b26e7b
Ricky Zhou <ricky@fedoraproject.org>
2010-11-01 21:41:40 -0400

    Release version 0.2.

--- __init__.py
+++ __init__.py
@@ -37,17 +37,17 @@ import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = ""
+__version__ = '0.2'
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.authors.unknown
+__author__ = supybot.Author('Ricky Zhou', 'ricky', 'ricky@fedoraproject.org')
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/Notify/download'
+__url__ = 'https://fedorahosted.org/released/supybot-notify/'
 
 import config
 import plugin
